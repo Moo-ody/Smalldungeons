f7dc34878b7a01eacecb0800c865254760027539
SubAt0mic <pokemonjunkbox@gmail.com>
2025-08-05 19:28:54 -0400

    sized string improvements

--- src/server/utils/sized_string.rs
+++ src/server/utils/sized_string.rs
@@ -1,13 +1,33 @@
 use crate::net::packets::packet_write::PacketWrite;
+use std::ops::Deref;
 
 /// [String] with a character size limit of N.
-/// All characters after that size will be truncated.
+/// All characters after that size will be truncated on creation.
+///
+/// This implements deref, which means any str operation will work on it.
+/// if you need the owned string, use .into_owned() or .to_owned()
 #[derive(Clone, Debug, Eq, Hash, PartialEq)]
 pub struct SizedString<const N: usize>(String);
+// if string is made public, you could change it and the size limit can't be enforced.
 
 impl<const N: usize> SizedString<N> {
+    pub fn truncated_owned(mut text: String) -> Self {
+        // cant just compare lengths because of how utf works >:(
+        for (index, (byte_position, _)) in text.char_indices().enumerate() {
+            if index == N {
+                text.truncate(byte_position);
+                return Self(text)
+            }
+        }
+        Self(text)
+    }
+
     pub fn truncated(text: &str) -> Self {
         Self(text.chars().take(N).collect::<String>())
+    }
+
+    pub fn into_owned(self) -> String {
+        self.0
     }
 
     pub fn as_str(&self) -> &str {
@@ -15,9 +35,11 @@ impl<const N: usize> SizedString<N> {
     }
 }
 
-impl<const N: usize> PacketWrite for SizedString<N> {
-    fn write(&self, writer: &mut Vec<u8>) {
-        self.0.write(writer);
+impl<const N: usize> Deref for SizedString<N> {
+    type Target = str;
+
+    fn deref(&self) -> &Self::Target {
+        self.as_str()
     }
 }
 
@@ -29,6 +51,19 @@ impl<const N: usize> From<&str> for SizedString<N> {
 
 impl<const N: usize> From<String> for SizedString<N> {
     fn from(text: String) -> Self {
-        Self::truncated(&text)
+        Self::truncated_owned(text)
+    }
+}
+
+impl<const N: usize> PacketWrite for SizedString<N> {
+    fn write(&self, writer: &mut Vec<u8>) {
+        self.0.write(writer);
+    }
+}
+
+
+impl<const N: usize> From<SizedString<N>> for String {
+    fn from(text: SizedString<N>) -> Self {
+        text.into_owned()
     }
 }
